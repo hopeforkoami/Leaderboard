9cc0b066623b7a637727caaba36a3514234e2444
hopeforkoami <hopeforkoami@gmail.com>
2022-10-13 07:21:56 +0000

    Connect with API

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 import _ from 'lodash'; // eslint-disable-line
+import Game from './modules/game.js';
 import './style.css';
-import './modules/game.js';
+
+const game = new Game()
+game.loadGameData()
+game.add('tester1',1000)
+game.loadGameData()
--- src/modules/game.js
+++ src/modules/game.js
@@ -1,12 +1,45 @@
 import Score from './score.js';
+import Service from './service.js';
 
 export default class Game {
   scores = []
-
-  generateId = () => this.scores[this.scores.length - 1].index + 1
+  id=''
+  api=''
+  constructor(){
+    this.id = JSON.parse(localStorage.getItem('gameID')) || ''
+    this.api = new Service();
+  }
+  loadGameData= async()=>{
+    if(!(this.id.length>0)){
+      try{
+        const rep = await this.api.createGame("new System Game")
+        this.id = rep.result.split(' ')[3]
+        localStorage.setItem('gameID',JSON.stringify(this.id))
+        this.loadGameData()
+      }catch(e){
+        console.log('error in game  creation'+e)
+      }
+       
+    }
+    else{
+      const rep = await this.api.loadGameData(this.id)
+      console.log("here is the game id "+this.id)
+      this.scores = rep.result
+    }
+  }
+  generateId = () => {
+    if(this.scores.length>0){
+      return this.scores[this.scores.length - 1].index + 1
+    }
+    else{
+      return 1
+    }
+    
+  }
 
   add = (name, score) => {
     this.scores.push(new Score(this.generateId(), name, score));
+    this.api.addNewScore(this.id,name, score)
   }
 
   generateContent = () => {
