1510443ea1dcce4ed15a4a496772046e71f016ae
hopeforkoami <hopeforkoami@gmail.com>
2022-10-13 19:03:48 +0000

    Correct linter errors

--- src/index.js
+++ src/index.js
@@ -2,34 +2,29 @@ import _ from 'lodash'; // eslint-disable-line
 import Game from './modules/game.js';
 import './style.css';
 
-const game = new Game()
-const scoreList = document.querySelector('.score_list_content')
-const refreshButton = document.querySelector('.refresh_button')
-const addButton = document.querySelector('.submit_button')
-const userName = document.querySelector('.form_name')
-const userScore = document.querySelector('.form_score')
+const game = new Game();
+const scoreList = document.querySelector('.score_list_content');
+const refreshButton = document.querySelector('.refresh_button');
+const addButton = document.querySelector('.submit_button');
+const userName = document.querySelector('.form_name');
+const userScore = document.querySelector('.form_score');
 
+const refreshScoresList = () => {
+  game.loadGameData().then((response) => {
+    scoreList.innerHTML = '';
+    scoreList.innerHTML = response;
+  });
+};
 
-const refreshScoresList = ()=>{
-  game.loadGameData().then((response)=>{
-    scoreList.innerHTML=""
-    scoreList.innerHTML = response
-    console.log(game.scores)
-  })
-}
-
-const addScore = ()=>{
-
-  if(!isNaN(userScore.value) ){
-    game.add(userName.value,userScore.value).then((response)=>{
-      refreshScoresList()
-      userName.value=''
-      userScore.value=''
-    })
-    
+const addScore = () => {
+  if (!Number.isNaN(userScore.value)) {
+    game.add(userName.value, userScore.value).then(() => {
+      refreshScoresList();
+      userName.value = '';
+      userScore.value = '';
+    });
   }
-   
-}
+};
 document.addEventListener('DOMContentLoaded', refreshScoresList);
-refreshButton.addEventListener('click',refreshScoresList)
-addButton.addEventListener('click',addScore)
+refreshButton.addEventListener('click', refreshScoresList);
+addButton.addEventListener('click', addScore);
--- src/modules/game.js
+++ src/modules/game.js
@@ -3,12 +3,16 @@ import Service from './service.js';
 
 export default class Game {
   scores = []
+
   id=''
+
   api=''
-  constructor(){
-    this.id = JSON.parse(localStorage.getItem('gameID')) || ''
+
+  constructor() {
+    this.id = JSON.parse(localStorage.getItem('gameID')) || '';
     this.api = new Service();
   }
+
   generateContent = () => {
     let content = '';
     this.scores.forEach((score, index) => {
@@ -16,48 +20,36 @@ export default class Game {
     });
     return content;
   }
-  loadGameData= async()=>{
-    if(!(this.id.length>0)){
-      try{
-        const rep = await this.api.createGame("new System Game")
-        this.id = rep.result.split(' ')[3]
-        localStorage.setItem('gameID',JSON.stringify(this.id))
-        this.loadGameData()
-      }catch(e){
-        console.log('error in game  creation'+e)
+
+  loadGameData= async () => {
+    if (!(this.id.length > 0)) {
+      const rep = await this.api.createGame('new System Game');
+      const tempArray = rep.result.split(' ');
+        this.id = tempArray[3];// eslint-disable-line
+      localStorage.setItem('gameID', JSON.stringify(this.id));
+      this.loadGameData();
+    } else {
+      const rep = await this.api.loadGameData(this.id);
+      if (rep.result.length > 0) {
+        this.scores = [];
+        rep.result.forEach((score, index) => {
+          this.scores.push(new Score(index, score.user, score.score));
+        });
       }
-       
     }
-    else{
-      const rep = await this.api.loadGameData(this.id)
-      console.log("here is the game id "+this.id)
-      if(rep.result.length>0){
-        this.scores = []
-        rep.result.forEach((score,index)=>{
-          this.scores.push(new Score(index,score.user,score.score))
-        })
-      }
-      return this.generateContent()
-    }
+    return this.generateContent();
   }
+
   generateId = () => {
-    if(this.scores.length>0){
-      return this.scores[this.scores.length - 1].index + 1
+    if (this.scores.length > 0) {
+      return this.scores[this.scores.length - 1].index + 1;
     }
-    else{
-      return 1
-    }
-    
+
+    return 1;
   }
 
-  add = async(name, score) => {
-    this.loadGameData()
-    await this.api.addNewScore(this.id,name, score).then((response)=>{
-      return true
-    })
-    
+  add = async (name, score) => {
+    this.loadGameData();
+    await this.api.addNewScore(this.id, name, score).then(() => true);
   }
-
-  
-
 }
